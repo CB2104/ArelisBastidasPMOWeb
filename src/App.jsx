964788370c237bdddc464aa6db888f6305ca1d67
CB2104 <141430425+CB2104@users.noreply.github.com>
2025-12-10 21:02:20 -0300

    Contacto-Updt

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Hero } from './components/hero/Hero';
 import { Parallax } from './components/parallax/Parallax';
 import { Services } from './components/services/Services';
 import { Portfolio } from './components/portfolio/Portfolio';
+import { Contact } from './components/contact/Contact';
 import './App.scss'
 
 const App = () => {
@@ -15,7 +16,7 @@ const App = () => {
     <section><Services/></section>
     <section id='Portfolio'><Parallax type="portfolio"/></section>
     <Portfolio/>
-    <section id='Contact'>Contact</section>
+    <section id='Contact'><Contact/></section>
     {/* <Test/>
     <Test/> */}
   </div>;
